ccecc000372c4f43d1a8c5cbd3833bd18ef2eb79
@vincent.gauchevertu <vinzmass@gmail.com>
2020-07-05 22:12:49 +0200

    Update JS files

--- public/js/script.js
+++ public/js/script.js
@@ -5,7 +5,13 @@
         if ($('.navigation').offset().top > 100) {
             $('.navigation').addClass('fixed-nav');
             $('.navigation').removeClass('fixed-nav-none');
-        } else {
+
+        }
+        else if ($('.navigation').offset().top > 0) {
+            $('.sticky-message').delay(1000).fadeOut(500);
+
+        }
+        else {
             $('.navigation').removeClass('fixed-nav');
             $('.navigation').addClass('fixed-nav-none');
         }
