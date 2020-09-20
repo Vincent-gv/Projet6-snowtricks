485c8d6b6f6bcf959e9be68f370133db74c2ffb5
Vincent-gv <44733849+Vincent-gv@users.noreply.github.com>
2020-09-20 23:19:54 +0200

    Do unit tests (#72)
    * Do unit tests

--- tests/Service/VideoPlatformServiceTest.php
+++ tests/Service/VideoPlatformServiceTest.php
@@ -1,23 +1,62 @@
 <?php
-//
-//namespace App\Tests\Services;
-//
-//use App\Entity\VideoTrick;
-//use App\Service\VideoPlatformService;
-//use PHPUnit\Framework\TestCase;
-//
-//class VideoPlatformServiceTest extends TestCase
-//{
-//    public function testParse()
-//    {
-//        $video = new VideoTrick();
-//        $video->setUrl('hthththt');
-//
-//        $videoPlatform = new VideoPlatformService();
-//
-//        $result = $videoPlatform->parse($video);
-//
-//        // assert that your calculator added the numbers correctly!
-//        $this->assertEquals(42, $result);
-//    }
-//}
+
+namespace App\Tests\Service;
+
+use App\Entity\VideoTrick;
+use App\Service\VideoPlatformService;
+use PHPUnit\Framework\TestCase;
+
+class VideoPlatformServiceTest extends TestCase
+{
+    public function testYoutubeLink()
+    {
+        $videoPlatformService = new VideoPlatformService();
+        $videoTrick = new VideoTrick();
+
+        $videoTrick->setUrl('https://www.youtube.com/watch?v=qybcQBiP7t0');
+
+        $videoPlatformService->parse($videoTrick);
+
+        $this->assertEquals('youtube', $videoTrick->getPlatformName());
+        $this->assertEquals('qybcQBiP7t0', $videoTrick->getPlatformId());
+    }
+
+    public function testYoutubeShortLink()
+    {
+        $videoPlatformService = new VideoPlatformService();
+        $videoTrick = new VideoTrick();
+
+        $videoTrick->setUrl('https://youtu.be/qybcQBiP7t0');
+
+        $videoPlatformService->parse($videoTrick);
+
+        $this->assertEquals('youtube', $videoTrick->getPlatformName());
+        $this->assertEquals('qybcQBiP7t0', $videoTrick->getPlatformId());
+    }
+
+    public function testYoutubeIframeLink()
+    {
+        $videoPlatformService = new VideoPlatformService();
+        $videoTrick = new VideoTrick();
+
+        $videoTrick->setUrl('https://www.youtube.com/embed/qybcQBiP7t0');
+
+        $videoPlatformService->parse($videoTrick);
+
+        $this->assertEquals('youtube', $videoTrick->getPlatformName());
+        $this->assertEquals('qybcQBiP7t0', $videoTrick->getPlatformId());
+    }
+
+    public function testBadVideoLink()
+    {
+        $videoPlatformService = new VideoPlatformService();
+        $videoTrick = new VideoTrick();
+
+        $videoTrick->setUrl('https://www.google.com');
+
+        $videoPlatformService->parse($videoTrick);
+
+        $this->assertEquals('youtube', $videoTrick->getPlatformName());
+        $this->assertEquals('qybcQBiP7t0', $videoTrick->getPlatformId());
+    }
+}
