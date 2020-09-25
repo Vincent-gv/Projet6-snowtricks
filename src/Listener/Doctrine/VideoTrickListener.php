56a7c558fdfd35b37b3d40728308b6365ef5b834
Vincent-gv <44733849+Vincent-gv@users.noreply.github.com>
2020-09-25 23:34:08 +0200

    Add Bad URL Exception (#78)

--- src/Listener/Doctrine/VideoTrickListener.php
+++ src/Listener/Doctrine/VideoTrickListener.php
@@ -4,6 +4,7 @@ namespace App\Listener\Doctrine;
 
 use App\Entity\VideoTrick;
 use App\Service\VideoPlatformService;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
 
 class VideoTrickListener
@@ -19,6 +20,11 @@ class VideoTrickListener
     }
 
     public function prePersist(VideoTrick $videoTrick, LifecycleEventArgs $eventArgs)
+    {
+        $this->videoPlatformService->parse($videoTrick);
+    }
+
+    public function preUpdate(VideoTrick $videoTrick, PreUpdateEventArgs $eventArgs)
     {
         $this->videoPlatformService->parse($videoTrick);
     }
